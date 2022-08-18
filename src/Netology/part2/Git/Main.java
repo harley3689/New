d4fbb48c2fb117f144ab7b5d712558bda303ea4b
harley <harley3689@gmail.com>
2022-08-18 18:01:35 +0300

    files restored

--- src/Netology/part2/Git/Main.java
+++ src/Netology/part2/Git/Main.java
@@ -2,8 +2,7 @@ package Netology.part2.Git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
-
+        SalesManager amount = new SalesManager(new int[]{20, 50, 10, 15, 8});
+        System.out.println(amount.max());
     }
-
 }
