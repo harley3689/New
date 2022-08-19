40d630fb5698b6638f0d2be07cd47439ff219676
harley <harley3689@gmail.com>
2022-08-19 16:28:55 +0300

    Long

--- src/Netology/part2/Git/Main.java
+++ src/Netology/part2/Git/Main.java
@@ -2,7 +2,7 @@ package Netology.part2.Git;
 
 public class Main {
     public static void main(String[] args) {
-        SalesManager amount = new SalesManager(new int[]{20, 35, 10, 15, 8});
+        SalesManager amount = new SalesManager(new long[]{20, 35, 10, 15, 8});
         System.out.println(amount.max());
     }
 }
