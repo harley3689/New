133620988b166f6ce5ae3a015cdc25b358aea76e
harley <harley3689@gmail.com>
2022-08-19 17:57:43 +0300

    medium add

--- src/Netology/part2/Git/Main.java
+++ src/Netology/part2/Git/Main.java
@@ -3,6 +3,7 @@ package Netology.part2.Git;
 public class Main {
     public static void main(String[] args) {
         SalesManager amount = new SalesManager(new int[]{20, 35, 10, 15, 8});
-        System.out.println(amount.max());
+        System.out.println("Large:"+amount.max()+"\n"+"Medium:"+amount.medium());
+
     }
 }
